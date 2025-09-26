53b9608e6e6fee6ea3d459e5b2c4e5f2bbe02697
gonzaleznoelia158 <gonzaleznoelia158@gmail.com>
2025-09-25 22:52:39 -0300

    Add files via upload

--- tarea.js
+++ tarea.js
@@ -14,24 +14,42 @@ function editartarea(tarea) {
     rl.question(`Titulo actual (${tarea.titulo}): `, function(nuevoTitulo) {
         if (nuevoTitulo.length > 0) tarea.titulo = nuevoTitulo;
         rl.question(`Descripcion actual (${tarea.descripcion}): `, function(nuevaDesc) {
-            if (nuevaDesc === '--') {
-                tarea.descripcion = '';
-            } else if (nuevaDesc.length > 0) {
-                tarea.descripcion = nuevaDesc;
-            }
-            rl.question(`Estado actual (${tarea.estado}) [pendiente/en curso/terminada]: `, function(nuevoEstado) {
-                if (["pendiente","en curso","terminada"].includes(nuevoEstado.toLowerCase())) {
-                    tarea.estado = nuevoEstado.toLowerCase();
+            if (nuevaDesc === '--') tarea.descripcion = '';
+            else if (nuevaDesc.length > 0) tarea.descripcion = nuevaDesc;
+            console.log("Estado actual: ", tarea.estado); 
+            console.log("1. pendiente");
+            console.log("2. en curso");
+            console.log("3. terminada");
+            rl.question("Seleccione una opción: ", function(opcionEstado) {
+                switch(opcionEstado){
+                    case "1": 
+                    tarea.estado = "pendiente";
+                     break;
+                    case "2":
+                         tarea.estado = "en curso";
+                          break;
+                    case "3":
+                         tarea.estado = "terminada";
+                          break;
                 }
                 rl.question(`Vencimiento actual (${tarea.vencimiento}): `, function(nuevoVenc) {
-                    if(nuevoVenc === '--') {
-                        tarea.vencimiento = null;
-                    } else if (nuevoVenc.length > 0) {
-                        tarea.vencimiento = new Date(nuevoVenc);
-                    }
-                    rl.question(`Dificultad actual (${tarea.dificultad}) [facil/medio/dificil]: `, function(nuevaDif) {
-                        if (["facil","medio","dificil"].includes(nuevaDif.toLowerCase())) {
-                            tarea.dificultad = nuevaDif.toLowerCase();
+                    if(nuevoVenc === '--') tarea.vencimiento = null;
+                    else if (nuevoVenc.length > 0) tarea.vencimiento = new Date(nuevoVenc);
+                    console.log("Dificultad actual:", tarea.dificultad);
+                    console.log("1. facil");
+                    console.log("2. medio");
+                    console.log("3. dificil");
+                    rl.question("Seleccione una opción: ", function(opcionDificultad) {
+                        switch(opcionDificultad){
+                            case "1":
+                                 tarea.dificultad = "facil";
+                                  break;
+                            case "2":
+                                 tarea.dificultad = "medio";
+                                  break;
+                            case "3":
+                                 tarea.dificultad = "dificil";
+                                  break;
                         }
                         console.clear();
                         console.log("Tarea actualizada correctamente:");
@@ -44,12 +62,12 @@ function editartarea(tarea) {
                         console.log(`Dificultad: ${tarea.dificultad}`);
                         rl.question("0 para volver a la lista de tareas, cualquier otra tecla para volver al menú principal: ", function(respuesta) {
                             switch(respuesta) {
-                                case "0":
-                                    vertareas();
-                                    break;
+                                case "0": 
+                                 vertareas();
+                                  break;
                                 default:
-                                    menu();
-                                    break;
+                                     menu();
+                                      break;
                             }
                         });
                     });
@@ -64,30 +82,26 @@ function mostrarListaYDetalles(lista, volverFunc = menu) {
         rl.question("0 para volver, cualquier otra tecla para menú principal: ", function(respuesta){
             switch(respuesta){
                 case "0":
-                    vertareas();
-                    break;
+                     vertareas();
+                      break;
                 default:
-                    menu();
-                    break;
+                     menu();
+                      break;
             }
         });
         return;
     }
     for(let i = 0; i < lista.length; i++){
-        console.log(`${lista[i].id} - ${lista[i].titulo}`);
+        console.log(` ${lista[i].id} - ${lista[i].titulo}`);
     }
     rl.question("Ingrese el ID de la tarea para ver detalles o 0 para volver: ", function(idtarea){
-        if(idtarea === "0"){
-            volverFunc();
-            return;
-        }
+        if(idtarea === "0")
+            { volverFunc(); return; }
         const idnum = parseInt(idtarea);
         let tarea = null;
         for(let i = 0; i < lista.length; i++){
-            if(lista[i].id === idnum){
-                tarea = lista[i];
-                break;
-            }
+            if(lista[i].id === idnum)
+                { tarea = lista[i]; break; }
         }
         if(tarea){
             console.clear();
@@ -105,14 +119,14 @@ function mostrarListaYDetalles(lista, volverFunc = menu) {
             rl.question("Seleccione una opción: ", function(opcion){
                 switch(opcion){
                     case "1":
-                        editartarea(tarea);
-                        break;
+                         editartarea(tarea); 
+                         break;
                     case "0":
-                        mostrarListaYDetalles(lista, volverFunc);
-                        break;
+                         mostrarListaYDetalles(lista, volverFunc);
+                          break;
                     default:
-                        menu();
-                        break;
+                         menu();
+                          break;
                 }
             });
         } else {
@@ -120,11 +134,11 @@ function mostrarListaYDetalles(lista, volverFunc = menu) {
             rl.question("0 para volver a la lista, otra tecla para menú principal: ", function(respuesta){
                 switch(respuesta){
                     case "0":
-                        mostrarListaYDetalles(lista, volverFunc);
-                        break;
+                         mostrarListaYDetalles(lista, volverFunc);
+                          break;
                     default:
-                        menu();
-                        break;
+                         menu();
+                          break;
                 }
             });
         }
@@ -137,16 +151,16 @@ function vertareas(){
         rl.question("Desea agregar una tarea? (1. Si / 0. No)", function(respuesta){
             switch(respuesta){
                 case "1":
-                    agregartarea();
-                    break;
+                     agregartarea();
+                      break;
                 case "0":
-                    menu();
-                    break;
+                     menu();
+                      break;
                 default:
-                    console.clear();
-                    console.log("Opcion no valida");
-                    vertareas();
-                    break;
+                     console.clear(); 
+                     console.log("Opcion no valida");
+                      vertareas();
+                       break;
             }
         });
     }
@@ -158,53 +172,65 @@ function vertareas(){
         console.log("4. Terminadas");
         console.log("0. Volver al menu principal");
         rl.question("Seleccione una opcion: ", function(opcion){
+            let lista = [];
             switch(opcion){
                 case "1":
                     console.clear();
-                    mostrarListaYDetalles(tareas);
+                    lista = tareas;
                     break;
                 case "2":
                     console.clear();
-                    mostrarListaYDetalles(tareas.filter(t => t.estado === "pendiente"));
+                    for (let i=0; i < tareas.length;i++)
+                    { if (tareas[i].estado === "pendiente")
+                    lista.push (tareas[i]); }
                     break;
                 case "3":
                     console.clear();
-                    mostrarListaYDetalles(tareas.filter(t => t.estado === "en curso"));
+                    for (let i=0; i < tareas.length;i++)
+                    { if (tareas[i].estado === "en curso")
+                    lista.push(tareas[i]); }
                     break;
                 case "4":
                     console.clear();
-                    mostrarListaYDetalles(tareas.filter(t => t.estado === "terminada"));
+                    for(let i=0; i < tareas.length; i++)
+                    { if (tareas[i].estado === "terminada") 
+                    lista.push(tareas[i]);}
                     break;
                 case "0":
-                    menu();
-                    break;
+                     menu();
+                     return;
                 default:
-                    console.clear();
-                    console.log("Opcion no valida");
-                    vertareas();
-                    break;
+                     console.clear();
+                     console.log("Opcion no valida"); 
+                     vertareas();
+                      return;
             }
+            mostrarListaYDetalles(lista);
         });
     }
 }
 function buscartarea() {
-    console.clear();
-    rl.question("Ingrese el título o parte del título de la tarea: ", function(busqueda) {
+    rl.question("Ingrese el título de la tarea: ", function(busqueda) {
         if(busqueda.length === 0){
-            console.log("Debe ingresar algún texto para buscar.");
+            console.clear();
+            console.log("Debe ingresar algún titulo para buscar.");
             return buscartarea();
         }
-        let resultados = tareas.filter(t => t.titulo.toLowerCase().includes(busqueda.toLowerCase()));
+        let resultados = [];
+        const busquedaMinusculas = busqueda.toLowerCase();
+        for(let i=0;i<tareas.length;i++){
+            if(tareas[i].titulo.toLowerCase() === busquedaMinusculas) resultados.push(tareas[i]);
+        }
         if(resultados.length === 0){
             console.log("No se encontraron tareas con ese título.");
-            rl.question("0 para volver al menú principal, cualquier otra tecla para intentar otra búsqueda: ", function(respuesta){
+            rl.question("0 Para volver al menu, presione cualquier tecla para reintentar: ", function(respuesta){
                 switch(respuesta){
-                    case "0":
-                        menu();
-                        break;
-                    default:
-                        buscartarea();
-                        break;
+                    case "0": 
+                    menu(); 
+                    break;
+                    default: 
+                    buscartarea();
+                     break;
                 }
             });
         } else {
@@ -224,7 +250,12 @@ function agregartarea(){
         }
         rl.question("Ingrese la descripcion de la tarea: ", (descripcion) => {
             rl.question("Ingrese el vencimiento de la tarea (aaaa-mm-dd): ", (vencimientouser) => {
-                let vencimiento = vencimientouser ? new Date(vencimientouser) : null;
+                let vencimiento;
+                if(vencimientouser){
+                    vencimiento = new Date(vencimientouser);
+                } else {
+                    vencimiento = null;
+                }
                 console.log("Seleccione el estado de la tarea:");
                 console.log("1. Pendiente");
                 console.log("2. En curso");
@@ -233,17 +264,17 @@ function agregartarea(){
                     let estado = "pendiente";
                     switch(opcionEstado) {
                         case "1":
-                            estado = "pendiente";
-                            break;
+                         estado = "pendiente";
+                          break;
                         case "2":
-                            estado = "en curso";
-                            break;
+                             estado = "en curso";
+                              break;
                         case "3":
-                            estado = "terminada";
-                            break;
+                             estado = "terminada";
+                              break;
                         default:
-                            console.log("Opción no válida, se asignará 'pendiente' por defecto.");
-                            break;
+                          console.log("Opción no válida, se asignará 'pendiente' por defecto.");
+                           break;
                     }
                     console.log("Seleccione la dificultad de la tarea:");
                     console.log("1. Facil");
@@ -253,17 +284,17 @@ function agregartarea(){
                         let dificultad = "facil";
                         switch(opcionDificultad) {
                             case "1":
-                                dificultad = "facil";
-                                break;
+                                 dificultad = "facil";
+                                  break;
                             case "2":
-                                dificultad = "medio";
-                                break;
+                                 dificultad = "medio";
+                                  break;
                             case "3":
-                                dificultad = "dificil";
-                                break;
+                                 dificultad = "dificil";
+                                  break;
                             default:
-                                console.log("Opción no válida, se asignará 'facil' por defecto.");
-                                break;
+                                 console.log("Opción no válida, se asignará 'facil' por defecto.");
+                                  break;
                         }
                         id++;
                         const creacion = new Date();
@@ -294,27 +325,27 @@ function menu(){
     console.log("0. Salir");
     rl.question("Seleccione una opcion: ", function(opcion){
         switch(opcion){
-            case "1":
-                console.clear();
-                vertareas();
-                break;
-            case "2":
-                console.clear();
-                buscartarea();
-                break;
-            case "3":
-                console.clear();
-                agregartarea();
-                break;
-            case "0":
-                console.log("Saliendo...");
-                rl.close();
-                break;
-            default:
-                console.clear();
-                console.log("Opcion no valida");
-                menu();
-                break;
+            case "1": 
+            console.clear();
+             vertareas(); 
+             break;
+            case "2": 
+            console.clear();
+             buscartarea(); 
+             break;
+            case "3": 
+            console.clear(); 
+            agregartarea(); 
+            break;
+            case "0": 
+            console.log("Saliendo..."); 
+            rl.close(); 
+            break;
+            default: 
+            console.clear(); 
+            console.log("Opcion no valida");
+             menu();
+             break;
         }
     })
 };
